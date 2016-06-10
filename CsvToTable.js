15b82d2a7b650569f2c06cfa6a1c1a9a41ef50da
Yash Sharma <yash_sharma@seologistics.com>
2016-06-10 15:56:30 +0530

    Convert csv file to html table

---
+++ CsvToTable.js
@@ -0,0 +1,97 @@
+(function(){
+
+	// Constructor method
+	this.CsvToTable = function(){
+		this.csvFile = null;
+		this.async = null;
+		
+		var defaults = {
+			async: true
+		};
+
+		// Create options by extending defaults with the passed in arugments
+    	if (arguments[0] && typeof arguments[0] === "object") {
+      		this.options = extendDefaults(defaults, arguments[0]);
+    	}
+
+	}
+
+	CsvToTable.prototype.run = function() {
+		return buildTable.call(this);
+	}
+
+	function getCSV() {
+		try{
+			var csvfile = this.options.csvFile,
+			async = this.options.async;
+			return new Promise(function(resolve, reject) {
+				var request = new XMLHttpRequest();
+				request.open("GET", csvfile, async);
+				request.onload = function() {
+				    if (request.status == 200) {
+				        resolve(request.response);
+				    } else {
+				        reject(Error(request.statusText));
+				    }
+				};
+
+				request.onerror = function() {
+				 	reject(Error('Error fetching data.'));
+				};
+				request.send();
+			});	
+		}catch(err){
+			console.error(err);
+		}
+	}
+
+	function buildTable() {
+		getCSV.call(this).then(function(response){
+			var allRows = response.split(/\r?\n|\r/);
+	        var table = '<table>';
+	        for (var singleRow = 0; singleRow < allRows.length; singleRow++) {
+	            if (singleRow === 0) {
+	                table += '<thead>';
+	                table += '<tr>';
+	            } else {
+	                table += '<tr>';
+	            }
+	            var rowCells = allRows[singleRow].split(',');
+	            for(var rowCell = 0; rowCell < rowCells.length; rowCell++){
+	                if(singleRow === 0){
+	                    table += '<th>';
+	                    table += rowCells[rowCell];
+	                    table += '</th>';
+	                } else {
+	                    table += '<td>';
+	                    table += rowCells[rowCell];
+	                    table += '</td>';
+	                }
+	            }
+	            if (singleRow === 0) {
+	                table += '</tr>';
+	                table += '</thead>';
+	                table += '<tbody>';
+	            } else {
+	                table += '</tr>';
+	            }
+	        }
+	        table += '</tbody>';
+	        table += '</table>';
+
+	        document.body.innerHTML += table;
+		}, function(error){
+			console.error(error);
+		});
+	}
+
+	function extendDefaults(source, properties) {
+		for (var property in properties) {
+		    if (properties.hasOwnProperty(property)) {
+				source[property] = properties[property];
+		    }
+		}
+		return source;
+	}
+	
+}());
