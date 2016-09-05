d90f187aa3d3503b9fcef5fc428a172e9e6cc7bf
yasharma <yashsharma205@gmail.com>
2016-09-05 21:08:06 +0530

    Update code to handle empty lines

--- CsvToTable.js
+++ CsvToTable.js
@@ -33,16 +33,38 @@
 				 	reject(Error('Error fetching data.'));
 				};
 				request.send();
-			});	
+			});
 		}catch(err){
 			console.error(err);
 		}
 	}
 
+    function isNotEmpty(row) {
+        return row !== "";
+    }
+
+    // polyfill `.filter()` for ECMAScript <5.1
+    // `f` must be pure (not modify original array).
+    if (!Array.prototype.filter) {
+      Array.prototype.filter = function(f) {
+        "use strict";
+        var p = arguments[1];
+        var o = Object(this);
+        var len = o.length;
+        for (var i = 0; i < len; i++) {
+          if (i in o) {
+              var v = o[i];
+              f.call(p, v, i, o);
+          }
+        }
+
+        return this;
+      };
+    }
+
 	function buildTable() {
 		getCSV.call(this).then(function(response){
-			var allRows = response.split(/\r?\n|\r/);
-			clean.call(this, allRows);
+			var allRows = response.split(/\r?\n|\r/).filter(isNotEmpty);
 	        var table = '<table>';
 	        for (var singleRow = 0; singleRow < allRows.length; singleRow++) {
 	            if (singleRow === 0) {
@@ -79,16 +101,4 @@
 			console.error(error);
 		});
 	}
-
-	// Removes empty elements from an array
-	function clean(arr){
-		arr.filter(function(value, index){
-			var count = 0;
-			if(value === "") {
-				count++;
-				arr.splice(index,count); 
-			}
-		});
-	}
-	
 }());
