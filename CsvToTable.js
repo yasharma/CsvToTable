d59e6118b393b8d0a9f44dd55d5b6a72db6d75ec
Saskian <bluebottel@gmail.com>
2019-09-17 23:57:38 +0200

    Use the File API instead of xmlhttprequest since that is deprecated
    xmlhttprequests/fetch can no longer access server "local" files even on Firefox. The File API is a drop in replacement since it also works with promises. Added a standard file upload element to the index page. Also moved the styling to an external file for convenience.

--- CsvToTable.js
+++ CsvToTable.js
@@ -1,104 +1,90 @@
 (function(){
 
-	// Constructor method
-	this.CsvToTable = function(){
-		this.csvFile = null;
+  // Constructor method
+  this.CsvToTable = function(){
+    this.csvFileHandle = null;
 
-		// Create options by extending defaults with the passed in arugments
-    	if (arguments[0] && typeof arguments[0] === "object") {
-      		this.options = arguments[0];
-    	}
-
-	}
-
-	CsvToTable.prototype.run = function() {
-		return buildTable.call(this);
-	}
-
-	function getCSV() {
-		try{
-			var csvfile = this.options.csvFile;
-			return new Promise(function(resolve, reject) {
-				var request = new XMLHttpRequest();
-				request.open("GET", csvfile, true);
-				request.onload = function() {
-				    if (request.status == 200) {
-				        resolve(request.response);
-				    } else {
-				        reject(Error(request.statusText));
-				    }
-				};
-
-				request.onerror = function() {
-				 	reject(Error('Error fetching data.'));
-				};
-				request.send();
-			});
-		}catch(err){
-			console.error(err);
-		}
-	}
-
-    function isNotEmpty(row) {
-        return row !== "";
+    // Create options by extending defaults with the passed in arguments
+    if (arguments[0] && typeof arguments[0] === "object") {
+      this.options = arguments[0];
     }
 
-    // polyfill `.filter()` for ECMAScript <5.1
-    // `f` must be pure (not modify original array).
-    if (!Array.prototype.filter) {
-      Array.prototype.filter = function(f) {
-        "use strict";
-        var p = arguments[1];
-        var o = Object(this);
-        var len = o.length;
-        for (var i = 0; i < len; i++) {
-          if (i in o) {
-              var v = o[i];
-              f.call(p, v, i, o);
-          }
+  }
+
+  CsvToTable.prototype.run = function() {
+    return buildTable.call(this);
+  }
+
+  function getCSV() {
+    try{
+      
+      // Returns a Promise
+      return this.options.csvFileHandle.text();
+    }catch(err){
+      console.error(err);
+    }
+  }
+
+  function isNotEmpty(row) {
+    return row !== "";
+  }
+
+  // polyfill `.filter()` for ECMAScript <5.1
+  // `f` must be pure (not modify original array).
+  if (!Array.prototype.filter) {
+    Array.prototype.filter = function(f) {
+      "use strict";
+      var p = arguments[1];
+      var o = Object(this);
+      var len = o.length;
+      for (var i = 0; i < len; i++) {
+        if (i in o) {
+          var v = o[i];
+          f.call(p, v, i, o);
         }
+      }
 
-        return this;
-      };
-    }
+      return this;
+    };
+  }
 
-	function buildTable() {
-		getCSV.call(this).then(function(response){
-			var allRows = response.split(/\r?\n|\r/).filter(isNotEmpty);
-	        var table = '<table>';
-	        for (var singleRow = 0; singleRow < allRows.length; singleRow++) {
-	            if (singleRow === 0) {
-	                table += '<thead>';
-	                table += '<tr>';
-	            } else {
-	                table += '<tr>';
-	            }
-	            var rowCells = allRows[singleRow].split(',');
-	            for(var rowCell = 0; rowCell < rowCells.length; rowCell++){
-	                if(singleRow === 0){
-	                    table += '<th>';
-	                    table += rowCells[rowCell];
-	                    table += '</th>';
-	                } else {
-	                    table += '<td>';
-	                    table += rowCells[rowCell];
-	                    table += '</td>';
-	                }
-	            }
-	            if (singleRow === 0) {
-	                table += '</tr>';
-	                table += '</thead>';
-	                table += '<tbody>';
-	            } else {
-	                table += '</tr>';
-	            }
-	        }
-	        table += '</tbody>';
-	        table += '</table>';
-
-	        document.body.innerHTML += table;
-		}, function(error){
-			console.error(error);
-		});
+  function buildTable() {
+    getCSV.call(this).then(function(response){
+      var allRows = response.split(/\r?\n|\r/).filter(isNotEmpty);
+      var table = '<table>';
+      for (var singleRow = 0; singleRow < allRows.length; singleRow++) {
+	if (singleRow === 0) {
+	  table += '<thead>';
+	  table += '<tr>';
+	} else {
+	  table += '<tr>';
 	}
+	var rowCells = allRows[singleRow].split(',');
+	for(var rowCell = 0; rowCell < rowCells.length; rowCell++){
+	  if(singleRow === 0){
+	    table += '<th>';
+	    table += rowCells[rowCell];
+	    table += '</th>';
+	  } else {
+	    table += '<td>';
+	    table += rowCells[rowCell];
+	    table += '</td>';
+	  }
+	}
+	if (singleRow === 0) {
+	  table += '</tr>';
+	  table += '</thead>';
+	  table += '<tbody>';
+	} else {
+	  table += '</tr>';
+	}
+      }
+      table += '</tbody>';
+      table += '</table>';
+
+      document.body.innerHTML += table;
+    }, function(error){
+      console.error(error);
+    });
+  }
 }());
