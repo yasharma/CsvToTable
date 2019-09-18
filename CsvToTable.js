8e99efb26a12d5f4d9506463a3bf9d2696706af1
Saskian <bluebottel@gmail.com>
2019-09-18 04:05:50 +0200

    Spaces to tabs to adhere to the previous style

--- CsvToTable.js
+++ CsvToTable.js
@@ -1,90 +1,90 @@
 (function(){
 
-  // Constructor method
-  this.CsvToTable = function(){
-    this.csvFileHandle = null;
+	// Constructor method
+	this.CsvToTable = function(){
+		this.csvFileHandle = null;
 
-    // Create options by extending defaults with the passed in arguments
-    if (arguments[0] && typeof arguments[0] === "object") {
-      this.options = arguments[0];
-    }
+		// Create options by extending defaults with the passed in arguments
+		if (arguments[0] && typeof arguments[0] === "object") {
+			this.options = arguments[0];
+		}
 
-  }
+	}
 
-  CsvToTable.prototype.run = function() {
-    return buildTable.call(this);
-  }
+	CsvToTable.prototype.run = function() {
+		return buildTable.call(this);
+	}
 
-  function getCSV() {
-    try{
-      
-      // Returns a Promise
-      return this.options.csvFileHandle.text();
-    }catch(err){
-      console.error(err);
-    }
-  }
+	function getCSV() {
+		try{
+			
+			// Returns a Promise
+			return this.options.csvFileHandle.text();
+		}catch(err){
+			console.error(err);
+		}
+	}
 
-  function isNotEmpty(row) {
-    return row !== "";
-  }
+	function isNotEmpty(row) {
+		return row !== "";
+	}
 
-  // polyfill `.filter()` for ECMAScript <5.1
-  // `f` must be pure (not modify original array).
-  if (!Array.prototype.filter) {
-    Array.prototype.filter = function(f) {
-      "use strict";
-      var p = arguments[1];
-      var o = Object(this);
-      var len = o.length;
-      for (var i = 0; i < len; i++) {
-        if (i in o) {
-          var v = o[i];
-          f.call(p, v, i, o);
-        }
-      }
+	// polyfill `.filter()` for ECMAScript <5.1
+	// `f` must be pure (not modify original array).
+	if (!Array.prototype.filter) {
+		Array.prototype.filter = function(f) {
+			"use strict";
+			var p = arguments[1];
+			var o = Object(this);
+			var len = o.length;
+			for (var i = 0; i < len; i++) {
+				if (i in o) {
+					var v = o[i];
+					f.call(p, v, i, o);
+				}
+			}
 
-      return this;
-    };
-  }
+			return this;
+		};
+	}
 
-  function buildTable() {
-    getCSV.call(this).then(function(response){
-      var allRows = response.split(/\r?\n|\r/).filter(isNotEmpty);
-      var table = '<table>';
-      for (var singleRow = 0; singleRow < allRows.length; singleRow++) {
+	function buildTable() {
+		getCSV.call(this).then(function(response){
+			var allRows = response.split(/\r?\n|\r/).filter(isNotEmpty);
+			var table = '<table>';
+			for (var singleRow = 0; singleRow < allRows.length; singleRow++) {
 	if (singleRow === 0) {
-	  table += '<thead>';
-	  table += '<tr>';
+		table += '<thead>';
+		table += '<tr>';
 	} else {
-	  table += '<tr>';
+		table += '<tr>';
 	}
 	var rowCells = allRows[singleRow].split(',');
 	for(var rowCell = 0; rowCell < rowCells.length; rowCell++){
-	  if(singleRow === 0){
-	    table += '<th>';
-	    table += rowCells[rowCell];
-	    table += '</th>';
-	  } else {
-	    table += '<td>';
-	    table += rowCells[rowCell];
-	    table += '</td>';
-	  }
+		if(singleRow === 0){
+			table += '<th>';
+			table += rowCells[rowCell];
+			table += '</th>';
+		} else {
+			table += '<td>';
+			table += rowCells[rowCell];
+			table += '</td>';
+		}
 	}
 	if (singleRow === 0) {
-	  table += '</tr>';
-	  table += '</thead>';
-	  table += '<tbody>';
+		table += '</tr>';
+		table += '</thead>';
+		table += '<tbody>';
 	} else {
-	  table += '</tr>';
+		table += '</tr>';
 	}
-      }
-      table += '</tbody>';
-      table += '</table>';
+			}
+			table += '</tbody>';
+			table += '</table>';
 
-      document.body.innerHTML += table;
-    }, function(error){
-      console.error(error);
-    });
-  }
+			document.body.innerHTML += table;
+		}, function(error){
+			console.error(error);
+		});
+	}
 }());
